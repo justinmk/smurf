673e7ea8d6fc8f228d484cfe7d3bbc2580c56060
Justin M. Keyes <justinkz@gmail.com>
2013-01-17 12:55:22 -0500

    java: StringUtil#isEveryCharUnique

---
+++ java/StringUtil.java
@@ -0,0 +1,43 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class StringUtil {
+
+	// time:  O(n^2) for alphabet of size n
+	// space: constant
+	public static boolean isEveryCharUnique_naive(String s) {
+		if (null == s)
+			return true;
+		System.out.print("testing string: " + s + " ");
+		char[] chars = s.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			for (int j = i+1; j < chars.length; j++) {
+				if (chars[i] == chars[j]) {
+					System.out.println("| dup: " + chars[i]);
+					return false;
+				}
+			}			
+		}
+		System.out.println();
+		return true;
+	}
+	
+	// time:  O(n) for alphabet of size n
+	// space: O(n) for alphabet of size n
+	public static boolean isEveryCharUnique_linear_time(String s) {
+		if (null == s)
+			return true;
+		System.out.print("testing string: " + s + " ");
+		Set<Character> found = new HashSet<Character>();
+		char[] chars = s.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			if (!found.add(chars[i])) {
+				System.out.println("| dup: " + chars[i]);
+				return false;
+			}
+		}
+		System.out.println();
+		return true;
+	}
+
+}
---
+++ java/StringUtilTest.java
@@ -0,0 +1,69 @@
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class StringUtilTest {
+
+	@Test public void test_isEveryCharUnique_null() {
+		assertTrue(StringUtil.isEveryCharUnique_naive(null));
+	}
+
+	@Test public void test_isEveryCharUnique_1() {
+		assertTrue(StringUtil.isEveryCharUnique_naive(""));
+	}
+
+	@Test public void test_isEveryCharUnique_2() {
+		assertTrue(StringUtil.isEveryCharUnique_naive("a"));
+	}
+
+	@Test public void test_isEveryCharUnique_3() {
+		assertFalse(StringUtil.isEveryCharUnique_naive("aa"));
+	}
+
+	@Test public void test_isEveryCharUnique_4() {
+		//for (int i = 0; i < 300000; i ++)
+			assertTrue(StringUtil.isEveryCharUnique_naive("abcdefghijklmnopqrstuvwx"));
+	}
+	
+	@Test public void test_isEveryCharUnique_5() {
+		assertFalse(StringUtil.isEveryCharUnique_naive("abcdefghijklmnopqrstuvwxa"));
+	}
+
+	@Test public void test_isEveryCharUnique_6() {
+		assertFalse(StringUtil.isEveryCharUnique_naive("abcdefghijklwmnopqrstuvwx"));
+	}
+
+	@Test public void test_isEveryCharUnique_linear_time_null() {
+		assertTrue(StringUtil.isEveryCharUnique_linear_time(null));
+	}
+	
+	@Test public void test_isEveryCharUnique_linear_time_1() {
+		assertTrue(StringUtil.isEveryCharUnique_linear_time(""));
+	}
+
+	@Test public void test_isEveryCharUnique_linear_time_2() {
+		assertTrue(StringUtil.isEveryCharUnique_linear_time("a"));
+	}
+
+	@Test public void test_isEveryCharUnique_linear_time_3() {
+		assertFalse(StringUtil.isEveryCharUnique_linear_time("aa"));
+	}
+
+	@Test public void test_isEveryCharUnique_linear_time_4() {
+		//for (int i = 0; i < 300000; i ++)
+			assertTrue(StringUtil.isEveryCharUnique_linear_time("abcdefghijklmnopqrstuvwx"));
+	}
+	
+	@Test public void test_isEveryCharUnique_linear_time_5() {
+		assertFalse(StringUtil.isEveryCharUnique_linear_time("abcdefghijklmnopqrstuvwxa"));
+	}
+	
+	@Test public void test_isEveryCharUnique_linear_time_6() {
+		assertFalse(StringUtil.isEveryCharUnique_linear_time("abcdefghijklwmnopqrstuvwx"));
+	}
+
+}
