e69ea9607af8ee6d0cda2f9149f97dd45ef7de40
Justin M. Keyes <justinkz@gmail.com>
2013-01-27 19:45:24 -0500

    traverse_inOrder_iterative() passes unit tests

--- java/BinaryTreeNode.java
+++ java/BinaryTreeNode.java
@@ -1,109 +1,128 @@
-
 public class BinaryTreeNode<T> {
-	public static interface BinaryTreeNodeVisitor<T> {
-		void apply(BinaryTreeNode<T> n);
-	}
-	
-	private BinaryTreeNode<T> _left;
-	private BinaryTreeNode<T> _right;
-	private BinaryTreeNode<T> _parent;
-	private T data;
-	/** Creates a new leaf node. */
-	public BinaryTreeNode(T data) {
-		this(data, null, null);
-	}
-	/** Creates a new node. */
-	public BinaryTreeNode(T data, BinaryTreeNode<T> left, BinaryTreeNode<T> right) {
-		setData(data);
-		
-		if (null != left) {
-			setLeft(left);
-			left.setParent(this);
-		}
-		
-		if (null != right) {
-			setRight(right);
-			right.setParent(this);
-		}
-	}
-	public BinaryTreeNode<T> left() {
-		return _left;
-	}
-	public void setLeft(BinaryTreeNode<T> left) {
-		this._left = left;
-	}
-	public BinaryTreeNode<T> right() {
-		return _right;
-	}
-	public void setRight(BinaryTreeNode<T> right) {
-		this._right = right;
-	}
-	public BinaryTreeNode<T> parent() {
-		return _parent;
-	}
-	public void setParent(BinaryTreeNode<T> parent) {
-		this._parent = parent;
-	}
-	public T data() {
-		return data;
-	}
-	public void setData(T data) {
-		this.data = data;
-	}
-	public boolean hasChildren() {
-		return null != _left || null != _right;
-	}
+  public static interface BinaryTreeNodeVisitor<T> {
+    void apply(BinaryTreeNode<T> n);
+  }
 
-	//three types of depth-first traversal: pre-order, in-order and post-order
-	
-	//in-order traversal: L P R
-	public void traverse_inOrder_recursive(BinaryTreeNodeVisitor<T> visitor) {
-		if (null != left())
-			left().traverse_inOrder_recursive(visitor);
-		
-		visitor.apply(this);
-		
-		if (null != right())
-			right().traverse_inOrder_recursive(visitor);
-	}
-	
-	//in-order traversal: L P R
-	//
-	//break down the problem:
-	//  - _last_ step should be the _outermost_ loop: percolate up the ancestors
-	//  - first ("preorder") step: recurse left descendants
-	//  - next ("inorder") step: visit parent
-	//  - next ("postorder") step: visit parent's right node (go to _first_ step)
-	public void traverse_inOrder_iterative(BinaryTreeNodeVisitor<T> visitor) {
-		BinaryTreeNode<T> n = this;
-		
-		//outer loop traverses down a *left* subtree.
-		//after the left-subtree is processed, it moves up until it finds a right subtree.
-		do { 
-			//find the leftmost subtree
-			while (null != n.left()) {
-				n = n.left();
-			}
-			
-			visitor.apply(n);
+  private BinaryTreeNode<T> _left;
+  private BinaryTreeNode<T> _right;
+  private BinaryTreeNode<T> _parent;
+  private T data;
+  /** Creates a new leaf node. */
+  public BinaryTreeNode(T data) {
+    this(data, null, null);
+  }
+  /** Creates a new node. */
+  public BinaryTreeNode(T data, BinaryTreeNode<T> left, BinaryTreeNode<T> right) {
+    setData(data);
 
-			if (null != n.right()) {
-				n = n.right();
-				continue;
-			}
+    if (null != left) {
+      setLeft(left);
+      left.setParent(this);
+    }
 
-			if (null != n.parent().right()) {
-				visitor.apply(n.parent());
-				n = n.parent().right();
-				continue; //now we are in a right-subtree
-			}
+    if (null != right) {
+      setRight(right);
+      right.setParent(this);
+    }
+  }
+  public BinaryTreeNode<T> left() {
+    return _left;
+  }
+  public void setLeft(BinaryTreeNode<T> left) {
+    this._left = left;
+  }
+  public BinaryTreeNode<T> right() {
+    return _right;
+  }
+  public void setRight(BinaryTreeNode<T> right) {
+    this._right = right;
+  }
+  public BinaryTreeNode<T> parent() {
+    return _parent;
+  }
+  public void setParent(BinaryTreeNode<T> parent) {
+    this._parent = parent;
+  }
+  public T data() {
+    return data;
+  }
+  public void setData(T data) {
+    this.data = data;
+  }
+  public boolean hasChildren() {
+    return null != _left || null != _right;
+  }
 
-			//this subtree is done; percolate up to an unvisited right-subtree
-			while (null != n.parent() && (n == n.parent().right() || null == n.parent().right())) {
-				n = n.parent(); 
-			}
-			
-		} while (null != n && null != n.parent());
-	}
+  // three types of depth-first traversal: pre-order, in-order and post-order
+
+  // in-order traversal: L P R
+  public void traverse_inOrder_recursive(BinaryTreeNodeVisitor<T> visitor) {
+    if (null != left())
+      left().traverse_inOrder_recursive(visitor);
+
+    visitor.apply(this);
+
+    if (null != right())
+      right().traverse_inOrder_recursive(visitor);
+  }
+
+  // in-order traversal: L P R
+  //
+  // break down the problem:
+  // - _last_ step should be the _outermost_ loop: percolate up the ancestors
+  // - first ("preorder") step: recurse left descendants
+  // - next ("inorder") step: visit parent
+  // - next ("postorder") step: visit parent's right node (go to _first_ step)
+  public void traverse_inOrder_iterative(BinaryTreeNodeVisitor<T> visitor) {
+    BinaryTreeNode<T> n = this;
+
+    // part [1] of the outer loop traverses down the current left-subtree.
+    // after a left-subtree is processed, part [2] "backtracks" until it finds a right-subtree.
+    do {
+      //[1] drill down
+      
+      // find the leftmost subtree
+      while (null != n.left()) {
+        n = n.left();
+      }
+
+      if (null != n.right()) {
+        visitor.apply(n);
+        n = n.right();
+        continue;
+      }
+      
+      visitor.apply(n);
+      
+      if (null != n.parent() && null != n.parent().right() && n != n.parent().right()) {
+        visitor.apply(n.parent());
+        n = n.parent().right();
+        continue;
+      }
+      
+      //[2] backtracking
+
+      // this subtree is done; percolate up to an unvisited right-subtree
+      while (null != n.parent() && n == n.parent().right()) {
+        //we know the parent was already visited, because L-P-R.
+        n = n.parent();
+      }
+      
+      while (null != n.parent() && n == n.parent().left()) {
+        visitor.apply(n.parent());
+        if (null != n.parent().right()) {
+          n = n.parent().right();
+          break; // now we are in a right-subtree
+        }
+        n = n.parent();
+        if (null != n.parent() && n != n.parent().left()) {
+          while (null != n.parent() && n == n.parent().right()) {
+            n = n.parent();
+          }
+        }
+      }
+      
+    } while (null != n && null != n.parent());
+  }
 
 }
--- java/BinaryTreeTest.java
+++ java/BinaryTreeTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertFalse;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,94 +13,154 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class BinaryTreeTest {
 	
-	private BinaryTreeNode<Integer> tree1;
-	private BinaryTreeNode<String> tree2;
-	final Integer[] tree1_expected_inorder_seq = new Integer[] { 5, 4, 6, 3, 7, 2, 9, 8, 10, 1, 11, 0, 16, 15, 17, 14, 18, 13, 20, 19, 21, 12, 22 };
-	final String[] tree2_expected_inorder_seq = new String[] {"A","B","C","D","E","F","G","H","I"};
-	private ArrayList<Integer> visit_seq_int;
-	private ArrayList<String> visit_seq_string;
+  private ArrayList<Integer> visit_seq_int;
+  private ArrayList<String> visit_seq_string;
+  private BinaryTreeNode<Integer> tree1;
+  private final Integer[] tree1_inorder = new Integer[] {5,4,6,3,7,2,9,8,10,1,11,0,16,15,17,14,18,13,20,19,21,12,22};
+  private BinaryTreeNode<String> tree2;
+  private final String[] tree2_inorder = new String[] {"A","B","C","D","E","F","G","H","I"};
+  private BinaryTreeNode<String> allLeftNodes;
+  private final String[] allLeftNodes_inorder = new String[] {"D","C","B","A"};
+  private BinaryTreeNode<String> allRightNodes;
+  private final String[] allRightNodes_inorder = new String[] {"A","B","C","D"};
 
 	@SuppressWarnings({ "unchecked", "rawtypes" }) @Before public void setup() {
 		tree1 = new BinaryTreeNode(0, 
-				new BinaryTreeNode(1, 
+    				new BinaryTreeNode(1, 
 						new BinaryTreeNode(2, 
 								new BinaryTreeNode(3, 
-										new BinaryTreeNode(4, 
-												new BinaryTreeNode(5), 
-												new BinaryTreeNode(6)), 
-										new BinaryTreeNode(7)), 
-								new BinaryTreeNode(8, 
-										new BinaryTreeNode(9), 
-										new BinaryTreeNode(10))), 
-						new BinaryTreeNode(11)), 
-				new BinaryTreeNode(12, 
-						new BinaryTreeNode(13, 
-								new BinaryTreeNode(14, 
-										new BinaryTreeNode(15, 
-												new BinaryTreeNode(16), 
-												new BinaryTreeNode(17)), 
-										new BinaryTreeNode(18)), 
-								new BinaryTreeNode(19, 
-										new BinaryTreeNode(20), 
+  										new BinaryTreeNode(4, 
+  												new BinaryTreeNode(5), 
+  												new BinaryTreeNode(6)), 
+  										new BinaryTreeNode(7)), 
+  								new BinaryTreeNode(8, 
+  										new BinaryTreeNode(9), 
+  										new BinaryTreeNode(10))), 
+  						new BinaryTreeNode(11)), 
+					new BinaryTreeNode(12, 
+  						new BinaryTreeNode(13, 
+  								new BinaryTreeNode(14, 
+  										new BinaryTreeNode(15, 
+  												new BinaryTreeNode(16), 
+  												new BinaryTreeNode(17)), 
+  										new BinaryTreeNode(18)), 
+  								new BinaryTreeNode(19, 
+  										new BinaryTreeNode(20), 
 										new BinaryTreeNode(21))), 
 						new BinaryTreeNode(22)));
 
 		tree2 = new BinaryTreeNode("F", 
-				new BinaryTreeNode("B", 
-						new BinaryTreeNode("A"), 
-						new BinaryTreeNode("D",
-								new BinaryTreeNode("C"),
-								new BinaryTreeNode("E"))), 
-				new BinaryTreeNode("G", 
-						null,
-						new BinaryTreeNode("I", 
-								new BinaryTreeNode("H"),
-								null)));
-
+    				new BinaryTreeNode("B", 
+    				    new BinaryTreeNode("A"), 
+    				    new BinaryTreeNode("D",
+    				        new BinaryTreeNode("C"),
+    				        new BinaryTreeNode("E"))), 
+    				new BinaryTreeNode("G", 
+    				    null,
+    				    new BinaryTreeNode("I", 
+    				        new BinaryTreeNode("H"),
+    				        null)));
+		
+        allLeftNodes = new BinaryTreeNode("A", 
+            new BinaryTreeNode("B", 
+                new BinaryTreeNode("C",
+                    new BinaryTreeNode("D"),
+                    null), 
+                null),
+            null);		
+        
+        allRightNodes = new BinaryTreeNode("A", 
+            null,
+            new BinaryTreeNode("B", 
+                null,
+                new BinaryTreeNode("C",
+                    null,
+                    new BinaryTreeNode("D"))));
+        
 		visit_seq_int = new ArrayList<Integer>();
 		visit_seq_string = new ArrayList<String>();
 	}
 	
-	BinaryTreeNode.BinaryTreeNodeVisitor<Integer> getVisitor_int() {
-		return new BinaryTreeNode.BinaryTreeNodeVisitor<Integer>() {
-			@Override public void apply(BinaryTreeNode<Integer> n) {
-				visit_seq_int.add(n.data());
-				System.out.print(n.data() + " ");
-			}
-		};
+	@After public void teardown() {
+	  System.out.println();
 	}
-	
+
+	BinaryTreeNode.BinaryTreeNodeVisitor<Integer> getVisitor_int() {
+	  return new BinaryTreeNode.BinaryTreeNodeVisitor<Integer>() {
+	    @Override public void apply(BinaryTreeNode<Integer> n) {
+	      visit_seq_int.add(n.data());
+	      System.out.print(n.data() + " ");
+	    }
+	  };
+	}
+
 	BinaryTreeNode.BinaryTreeNodeVisitor<String> getVisitor_string() {
-		return new BinaryTreeNode.BinaryTreeNodeVisitor<String>() {
-			@Override public void apply(BinaryTreeNode<String> n) {
-				visit_seq_string.add(n.data());
-				System.out.print(n.data() + " ");
-			}
-		};
+	  return new BinaryTreeNode.BinaryTreeNodeVisitor<String>() {
+	    @Override public void apply(BinaryTreeNode<String> n) {
+	      visit_seq_string.add(n.data());
+	      System.out.print(n.data() + " ");
+	    }
+	  };
 	}
 
 	@Test public void test_traverseInOrder_recursive() {
-		tree1.traverse_inOrder_recursive(getVisitor_int());
-		assertTrue("actual visit sequence: " + Arrays.toString(visit_seq_int.toArray()), 
-				Arrays.equals(tree1_expected_inorder_seq, visit_seq_int.toArray()));
+	  tree1.traverse_inOrder_recursive(getVisitor_int());
+	  assertTrue("actual visit sequence: " + Arrays.toString(visit_seq_int.toArray()), 
+	      Arrays.equals(tree1_inorder, visit_seq_int.toArray()));
 	}
-	
+
 	@Test public void test_traverseInOrder_iterative() {
-		tree1.traverse_inOrder_iterative(getVisitor_int());
-		assertTrue("actual visit sequence: " + Arrays.toString(visit_seq_int.toArray()), 
-				Arrays.equals(tree1_expected_inorder_seq, visit_seq_int.toArray()));
+	  tree1.traverse_inOrder_iterative(getVisitor_int());
+	  assertTrue("actual visit sequence: " + Arrays.toString(visit_seq_int.toArray()), 
+	      Arrays.equals(tree1_inorder, visit_seq_int.toArray()));
 	}
 	
+	@Test public void test_traverseInOrder_recursive_singleNode() {
+	  new BinaryTreeNode<String>("A").traverse_inOrder_recursive(getVisitor_string());
+	  assertTrue("actual visit sequence: " + Arrays.toString(visit_seq_string.toArray()), 
+	      Arrays.equals(new String[] {"A"}, visit_seq_string.toArray()));
+	}
+
+	@Test public void test_traverseInOrder_iterative_singleNode() {
+	  new BinaryTreeNode<String>("A").traverse_inOrder_iterative(getVisitor_string());
+	  assertTrue("actual visit sequence: " + Arrays.toString(visit_seq_string.toArray()), 
+	      Arrays.equals(new String[] {"A"}, visit_seq_string.toArray()));
+	}
+
 	@Test public void test_traverseInOrder_recursive_2() {
-		tree2.traverse_inOrder_recursive(getVisitor_string());
-		assertTrue("actual visit sequence: " + Arrays.toString(visit_seq_string.toArray()), 
-				Arrays.equals(tree2_expected_inorder_seq, visit_seq_string.toArray()));
+	  tree2.traverse_inOrder_recursive(getVisitor_string());
+	  assertTrue("actual visit sequence: " + Arrays.toString(visit_seq_string.toArray()), 
+	      Arrays.equals(tree2_inorder, visit_seq_string.toArray()));
+	}
+
+	@Test public void test_traverseInOrder_iterative_2() {
+	  tree2.traverse_inOrder_iterative(getVisitor_string());
+	  assertTrue("actual visit sequence: " + Arrays.toString(visit_seq_string.toArray()), 
+	      Arrays.equals(tree2_inorder, visit_seq_string.toArray()));
 	}
 	
-	@Test public void test_traverseInOrder_iterative_2() {
-		tree2.traverse_inOrder_iterative(getVisitor_string());
-		assertTrue("actual visit sequence: " + Arrays.toString(visit_seq_string.toArray()), 
-				Arrays.equals(tree2_expected_inorder_seq, visit_seq_string.toArray()));
-	}
+    @Test public void test_traverseInOrder_recursive_allLeftNodes() {
+      allLeftNodes.traverse_inOrder_recursive(getVisitor_string());
+      assertTrue("actual visit sequence: " + Arrays.toString(visit_seq_string.toArray()), 
+          Arrays.equals(allLeftNodes_inorder, visit_seq_string.toArray()));
+    }
+
+    @Test public void test_traverseInOrder_iterative_allLeftNodes() {
+      allLeftNodes.traverse_inOrder_iterative(getVisitor_string());
+      assertTrue("actual visit sequence: " + Arrays.toString(visit_seq_string.toArray()), 
+          Arrays.equals(allLeftNodes_inorder, visit_seq_string.toArray()));
+    }
+    
+    @Test public void test_traverseInOrder_recursive_allRightNodes() {
+      allRightNodes.traverse_inOrder_recursive(getVisitor_string());
+      assertTrue("actual visit sequence: " + Arrays.toString(visit_seq_string.toArray()), 
+          Arrays.equals(allRightNodes_inorder, visit_seq_string.toArray()));
+    }
+
+    @Test public void test_traverseInOrder_iterative_allRightNodes() {
+      allRightNodes.traverse_inOrder_iterative(getVisitor_string());
+      assertTrue("actual visit sequence: " + Arrays.toString(visit_seq_string.toArray()), 
+          Arrays.equals(allRightNodes_inorder, visit_seq_string.toArray()));
+    }
 
 }
